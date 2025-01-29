d6e76728088273fbe3343599f284f7c01834ca72
JoabCoelh0 <josejoab003@gmail.com>
2025-01-29 16:28:09 -0300

    asd

--- assents/main/main.js
+++ assents/main/main.js
@@ -1,11 +1,1 @@
-const botao = document.getElementById("ariaCima");
-const heroSection = document.querySelector(".talaInicial");
-
-window.addEventListener("scroll", () => {
-    // Verifica se a hero section saiu da tela
-    if (window.scrollY > heroSection.clientHeight / 2) {
-        botao.classList.add("show");
-    } else {
-        botao.classList.remove("show");
-    }
-});
+s
